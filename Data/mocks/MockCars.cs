5c80ce1078505bc5cc5c00660c2cbaa4509347a3
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-27 01:56:56 -0400

    mocks

---
+++ Data/mocks/MockCars.cs
@@ -0,0 +1,80 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class MockCars : IAllCars
+    {
+
+        private readonly ICarsCategory _categoryCars = new MockCategory();
+
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car> {
+                    new Car {
+                        name = "Tesla Model S",
+                        shortDesc = "Быстрый автомобиль",
+                        longDesc = "Красивый, быстрый и очень тихий автомобиль компании Tesla",
+                        img = "https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/socialcard/MS.jpg",
+                        price = 45000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.First()
+                    },
+                    new Car {
+                        name = "Ford Fiesta",
+                        shortDesc = "Тихий и спокойный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "https://www.winnerauto.ua/uploads/gallery_photo/photo/0170/91.jpg",
+                        price = 11000,
+                        isFavourite = false,
+                        available = true,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car {
+                        name = "BMW M3",
+                        shortDesc = "Дерзкий и стильный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "https://img.tipcars.com/fotky_velke/33550669_9/2018/E/bmw-m3-top-m-performance-paket.jpg",
+                        price = 65000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car {
+                        name = "Mercedes C class",
+                        shortDesc = "Уютный и большой",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "https://0b38966e54e8bebf4a9b-c91cf87d0ed24f4939ca44173a86a2bc.ssl.cf1.rackcdn.com/55SWF8EBXKU284256/486f758369faedbf248aba14b75f9151.jpg",
+                        price = 40000,
+                        isFavourite = false,
+                        available = false,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car {
+                        name = "Nissan Leaf",
+                        shortDesc = "Бесшумный и экономный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "https://d2t6ms4cjod3h9.cloudfront.net/wp-content/uploads/2018/11/LEAF_Nissan_Energy_Share-source.jpg",
+                        price = 14000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.First()
+                    }
+                };
+            }
+        }
+        public IEnumerable<Car> getFavCars { get; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/mocks/MockCategory.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category> {
+                    new Category { categoryName = "Электромобили", desc = "Современный вид транспорта" },
+                    new Category { categoryName = "Классические автомобили", desc = "Машины с двигателем внутреннего згорания" }
+                };
+            }
+        }
+    }
+}
