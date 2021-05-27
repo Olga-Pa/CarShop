6e8afde4ef73c4ee3d52c436ae7d02ea4fd8b242
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-27 01:49:10 -0400

    Add project files.

---
+++ Data/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Car
+    {
+
+        public int id { set; get; }
+
+        public string name { set; get; }
+
+        public string shortDesc { set; get; }
+
+        public string longDesc { set; get; }
+
+        public string img { set; get; }
+
+        public ushort price { set; get; }
+
+        public bool isFavourite { set; get; }
+
+        public bool available { set; get; }
+
+        public int categoryID { set; get; }
+
+        public virtual Category Category { set; get; }
+
+    }
+}
---
+++ Data/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Models
+{
+    public class Category
+    {
+
+        public int id { set; get; }
+
+        public string categoryName { set; get; }
+
+        public string desc { set; get; }
+
+        public List<Car> cars { set; get; }
+
+    }
+}
