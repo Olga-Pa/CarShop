cc9c20a57a0d626038130aecc667fd4f5f6d5996
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-27 01:52:42 -0400

    interfaces

---
+++ Data/interfaces/IAllCars.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.interfaces
+{
+    public interface IAllCars
+    {
+
+        IEnumerable<Car> Cars { get; }
+        IEnumerable<Car> getFavCars { get; set; }
+        Car getObjectCar(int carId);
+
+    }
+}
---
+++ Data/interfaces/ICarsCategory.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.interfaces
+{
+    public interface ICarsCategory
+    {
+
+        IEnumerable<Category> AllCategories { get; }
+
+    }
+}
