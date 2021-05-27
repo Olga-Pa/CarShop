66584373782ba07a8900e3b3c3865df965ac9dcf
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-27 02:43:10 -0400

    works

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.interfaces;
+//using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iAllCars;
+            _allCategories = iCarsCat;
+        }
+
+
+       // public ViewResult List()
+        //{
+           // ViewBag.Title = "Страница с автомобилями";
+            //CarsListViewModel obj = new CarsListViewModel();
+           // obj.allCars = _allCars.Cars;
+           // obj.currCategory = "Автомобили";
+
+            //return View(obj);
+       // }
+
+
+    }
+}
