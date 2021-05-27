66ec8959e36f4fd8716420a9106fd159bed91a51
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-27 02:03:37 -0400

    AddTransient to startup

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data.interfaces;
+using Shop.Data.mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,9 @@ namespace CarShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IAllCars, MockCars>();
+            services.AddTransient<ICarsCategory, MockCategory>();
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
