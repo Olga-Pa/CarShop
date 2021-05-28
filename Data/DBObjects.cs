18cd09f4e87e4d1c0e2d8225459120bf3adc5070
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-28 04:11:43 -0400

    works

---
+++ Data/DBObjects.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data
+{
+    public class DBObjects
+    {
+        public static void Initial(AppDBContent content)
+        {
+
+            if (!content.Category.Any())
+                content.Category.AddRange(Categories.Select(c => c.Value));
+
+            if (!content.Car.Any())
+            {
+                content.AddRange(
+                    new Car
+                    {
+                        name = "Tesla Model S",
+                        shortDesc = "Быстрый автомобиль",
+                        longDesc = "Красивый, быстрый и очень тихий автомобиль компании Tesla",
+                        img = "/img/tesla.jpg",
+                        price = 45000,
+                        isFavourite = true,
+                        available = true,
+                        Category = Categories["Электромобили"]
+                    },
+                    new Car
+                    {
+                        name = "Ford Fiesta",
+                        shortDesc = "Тихий и спокойный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/fiesta.jpg",
+                        price = 11000,
+                        isFavourite = false,
+                        available = true,
+                        Category = Categories["Классические автомобили"]
+                    },
+                    new Car
+                    {
+                        name = "BMW M3",
+                        shortDesc = "Дерзкий и стильный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/m3.jpg",
+                        price = 65000,
+                        isFavourite = true,
+                        available = true,
+                        Category = Categories["Классические автомобили"]
+                    },
+                    new Car
+                    {
+                        name = "Mercedes C class",
+                        shortDesc = "Уютный и большой",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/mercedes.jpg",
+                        price = 40000,
+                        isFavourite = false,
+                        available = false,
+                        Category = Categories["Классические автомобили"]
+                    },
+                    new Car
+                    {
+                        name = "Nissan Leaf",
+                        shortDesc = "Бесшумный и экономный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/nissan.jpg",
+                        price = 14000,
+                        isFavourite = true,
+                        available = true,
+                        Category = Categories["Электромобили"]
+                    }
+                );
+            }
+
+            content.SaveChanges();
+
+        }
+
+        private static Dictionary<string, Category> category;
+        public static Dictionary<string, Category> Categories
+        {
+            get
+            {
+                if (category == null)
+                {
+                    var list = new Category[] {
+                        new Category { categoryName = "Электромобили", desc = "Современный вид транспорта" },
+                        new Category { categoryName = "Классические автомобили", desc = "Машины с двигателем внутреннего згорания" }
+                    };
+
+                    category = new Dictionary<string, Category>();
+                    foreach (Category el in list)
+                        category.Add(el.categoryName, el);
+                }
+
+                return category;
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,27 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Shop.Data;
 using Shop.Data.interfaces;
 using Shop.Data.mocks;
 using Shop.Data.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Shop
 {
     public class Startup
     {
 
+
         private IConfigurationRoot _confSting;
-        
-        public Startup(Microsoft.AspNetCore.Hosting.IWebHostEnvironment hostEnv)
+
+        public Startup(IHostingEnvironment hostEnv)
         {
             _confSting = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
         }
@@ -30,34 +30,26 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confSting.GetConnectionString("DefaultConnection")));
             services.AddTransient<IAllCars, CarRepository>();
             services.AddTransient<ICarsCategory, CategoryRepository>();
-
             services.AddMvc(options => options.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
-            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            using (var scope = app.ApplicationServices.CreateScope())
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+                DBObjects.Initial(content);
+            }
         }
     }
 }
