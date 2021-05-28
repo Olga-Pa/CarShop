9cb1d7a68892bba618f79698b54913b9fdd39723
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-28 03:08:15 -0400

    migrations

---
+++ Migrations/20210528070457_Initial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    categoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    desc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    shortDesc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    longDesc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    img = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    isFavourite = table.Column<bool>(type: "bit", nullable: false),
+                    available = table.Column<bool>(type: "bit", nullable: false),
+                    categoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Car_Category_categoryID",
+                        column: x => x.categoryID,
+                        principalTable: "Category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_categoryID",
+                table: "Car",
+                column: "categoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
